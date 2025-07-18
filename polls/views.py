d2bf505e965633685c4db4ceee1819ee587d3fe2
DanielNwolu <nwoludaniel.c@gmail.com>
2025-07-18 15:02:51 +0100

    completed up to part 3 of the first django app tutorial

---
+++ polls/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render,get_object_or_404
+from django.http import Http404
+
+from .models import Question
+
+# using template loader to load templates
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     template = loader.get_template("polls/index.html")
+#     context = {"latest_question_list": latest_question_list}
+#     return HttpResponse(template.render(context, request))
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+def detail(request,question_id):
+    try:
+        question = get_object_or_404(Question, pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
+
+def result(request, question_id):
+    response ="Your're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
